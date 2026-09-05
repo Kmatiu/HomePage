5e838286ab6fed4a6b16cf9eb5b106aec06d4e22
Kmatiu <camachico@gmail.com>
2026-09-05 18:42:24 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    propia carpeta, así que si se sube dentro de webs/ no vería ni images/ ni la
    portada, y la descarga sin conexión no serviría de nada. */
 
-var VERSION = 'v4';
+var VERSION = 'v5';
 var CACHE = 'juegos-' + VERSION;
 
 /* Lo mínimo para que la portada abra sin conexión. Se cachea solo al instalar,
